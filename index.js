c7decf0dd2ac923a96f49b7a50ba76bd2d7e87dd
br14nn <vituallabrian727@gmail.com>
2024-02-26 19:46:25 +0800

    error page done

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   let parsedURL = url.parse(req.url, true);
 
-  let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
+  let path = parsedURL.path.replace(/^\/+|\/+$/g, '');
 
   // console.log(path);
 
-  if (path === "") {
-    path = "index.html";
-  } else if (path === "portfolio") {
-    path = "portfolio.html";
-  } else if (path === "about") {
-    path = "about.html";
-  } else if (path === "unique") {
-    path = "unique.html";
-  } else if (path === "styles.css") {
-    path = "styles.css";
+  if (path === '') {
+    path = 'index.html';
+  } else if (path === 'portfolio') {
+    path = 'portfolio.html';
+  } else if (path === 'about') {
+    path = 'about.html';
+  } else if (path === 'unique') {
+    path = 'unique.html';
+  } else if (path === 'styles.css') {
+    path = 'styles.css';
   } else if (/\.png$/.test(path)) {
     path = path;
   } else if (/\.svg$/.test(path)) {
     path = path;
   } else {
-    path = "error.html";
+    path = 'error.html';
   }
 
-  let file = __dirname + "/public/" + path;
+  let file = __dirname + '/public/' + path;
 
   // console.log(file);
 
@@ -36,25 +36,23 @@ const server = http.createServer((req, res) => {
       res.writeHead(404);
       res.end();
     } else {
-      res.setHeader("X-Content-Type-Options", "nosniff");
+      res.setHeader('X-Content-Type-Options', 'nosniff');
       // console.log(path);
       switch (path) {
-        case "styles.css":
-          res.writeHead(200, { "Content-type": "text/css" });
+        case 'styles.css':
+          res.writeHead(200, { 'Content-type': 'text/css' });
           break;
-        case /\.svg$/.test(path):
-          res.writeHead(200, { "Content-Type": "image/svg+xml" });
         case /\.png$/.test(path):
-          res.writeHead(200, { "Content-type": "image/png" });
+          res.writeHead(200, { 'Content-type': 'image/png' });
           break;
         default:
-          res.writeHead(200, { "Content-type": "text/html" });
+          res.writeHead(200, { 'Content-type': 'text/html' });
       }
       res.end(content);
     }
   });
 });
 
-server.listen(3000, "localhost", () => {
-  console.log("Listening on port 3000");
+server.listen(3000, 'localhost', () => {
+  console.log('Listening on port 3000');
 });
