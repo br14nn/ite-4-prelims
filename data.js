fad543197b99358af3b75d7e93e9db2dda7f1fd9
br14nn <vituallabrian727@gmail.com>
2024-04-13 23:09:20 +0800

    using backend data in about page

---
+++ data.js
@@ -0,0 +1,27 @@
+const about = {
+  pfp: "profile-picture.png",
+  name: "Brian Velasco Vitualla",
+  career: "Web Developer",
+  intro:
+    "Meet Brian, a web wizard. Designs stunning sites with HTML, CSS, and JavaScript. Blending creativity and tech savvy for seamless user experiences.",
+  socials: [
+    {
+      img: "facebook-svgrepo-com.png",
+      url: "https://www.facebook.com/brianVVitualla",
+    },
+    {
+      img: "twitter-svgrepo-com.png",
+      url: "https://twitter.com/_bri__bri___",
+    },
+    {
+      img: "linkedin-svgrepo-com.png",
+      url: "https://www.linkedin.com/in/brianvvitualla/",
+    },
+    {
+      img: "github-svgrepo-com.png",
+      url: "https://github.com/br14nn",
+    },
+  ],
+};
+
+module.exports = { about };
--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-const express = require('express');
+const express = require("express");
+const { about } = require("./data");
+
 const app = express();
 
 const port = 3000;
 
-app.use(express.static('views'));
-app.set('view engine', 'ejs');
+app.use(express.static("views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/portfolio', (req, res) => {
-  res.render('portfolio');
+app.get("/portfolio", (req, res) => {
+  res.render("portfolio");
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    fullname: 'Brian Velasco Vitualla',
-    career: 'Web Developer Unta',
-    facebookLink: 'https://www.facebook.com/brianVVitualla',
-    twitterLink: 'https://twitter.com/_bri__bri___',
-    linkedInLink: 'https://www.linkedin.com/in/brianvvitualla/',
-    githubLink: 'https://github.com/br14nn',
+app.get("/about", (req, res) => {
+  res.render("about", {
+    pfp: about.pfp,
+    name: about.name,
+    career: about.career,
+    short_intro: about.intro,
+    socials: about.socials,
   });
 });
 
-app.get('/unique', (req, res) => {
-  res.render('unique.html');
+app.get("/unique", (req, res) => {
+  res.render("unique.html");
 });
 
 app.use((req, res) => {
-  res.status(404).render('error');
+  res.status(404).render("error");
 });
 
 app.listen(port, () => {
