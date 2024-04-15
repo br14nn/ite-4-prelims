0c86a4d50717b33734d9180864c6f1d592275ad8
br14nn <vituallabrian727@gmail.com>
2024-04-15 20:04:24 +0800

    updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { about, portfolio } = require("./data");
 
 const app = express();
 
-const port = 3000;
+const port = 4000;
 
 app.use(express.static("views"));
 app.set("view engine", "ejs");
